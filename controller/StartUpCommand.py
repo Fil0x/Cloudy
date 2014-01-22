fbf234d947f353081f4b922c204cc1ea929b5747
Fadi Samisti <fadisamisti@gmail.com>
2014-01-22 11:59:32 +0200

    Settings initialization.

--- controller/StartUpCommand.py
+++ controller/StartUpCommand.py
@@ -2,7 +2,9 @@ import sys
 if ".." not in sys.path:
     sys.path.append("..")
 
+import strings
 import version
+import AppFacade
 import model.modelProxy
 from view.Settings import Settings
 from view.view import SysTrayMediator
@@ -41,10 +43,11 @@ class StartUpCommand(puremvc.patterns.command.SimpleCommand, puremvc.interfaces.
         if not used_services:
             systray.show_message(self.no_service_title, self.no_service_body, duration=5000)
 
-        s = Settings(used_services, service_folders, p.get_general_settings())
+        stored_settings = p.get_general_settings()
+        s = Settings(used_services, service_folders, stored_settings)
         self.facade.registerMediator(SettingsMediator(s))
 
-        #The compact window must not have a taskbar entry, the solution to this is to draw it 
+        #TODO:The compact window must not have a taskbar entry, the solution to this is to draw it
         #through a dummy parent widget that is never shown.
         c = CompactWindow(p.get_services(), p.get_orientation(),
                           p.get_pos('Compact'), 0)
@@ -54,4 +57,24 @@ class StartUpCommand(puremvc.patterns.command.SimpleCommand, puremvc.interfaces.
                            p.get_size(), p.get_maximized(), 0, s)
         self.facade.registerMediator(DetailedWindowMediator(d))
 
+        #Initialize the UI components to behave according to the stored settings.
+        self.facade.sendNotification(AppFacade.AppFacade.SETTINGS_HISTORY_CLOSE_ON_SHARE,
+                                     stored_settings[strings.popup_checkbox])
+        self.facade.sendNotification(AppFacade.AppFacade.SETTINGS_DETAILED_MINIMIZE_ON_CLOSE,
+                                     stored_settings[strings.close_checkbox])
+
         proxy.start_uploads()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
